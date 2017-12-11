7d5d0ff3ff1cce371a7cd5acbbaf5962d7df7489
Braulio Diez <braulio.diez@gmail.com>
2017-12-11 20:30:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-const messageToDisplay = ` I have updated this on the client and should be the change that rules`;
+const messageToDisplay = ` Hey this change, we don't want it`;
 document.write(messageToDisplay);
